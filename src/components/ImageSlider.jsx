dcf30abfdbb55a0911e969820d282255dc0e7ca9
Abhishek Dhakal <abhishekdhakal1826@gmail.com>
2024-11-29 13:33:23 +0545

    changed into react + tailwind

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
-import "./style.css";
 
 export default function ImageSlider({ url, limit = 10, page = 1 }) {
   const [images, setImages] = useState();
@@ -11,7 +10,6 @@ export default function ImageSlider({ url, limit = 10, page = 1 }) {
   async function fetchImages(getUrl) {
     try {
       setLoading(true);
-
       const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
       const data = await response.json();
 
@@ -28,16 +26,14 @@ export default function ImageSlider({ url, limit = 10, page = 1 }) {
   useEffect(() => {
     if (url !== "") fetchImages(url);
   }, [url]);
-  // console.log(images);
+
   if (loading) {
-    return <div>Loading...Data is on the way.</div>;
+    return <div>Loading... Data is on the way.</div>;
   }
 
   if (error !== null) {
-    return <div> Error occured</div>;
+    return <div>Error occurred</div>;
   }
-
-  // console.log(error);
 
   function handlePrevious() {
     setSlide(slide === 0 ? images.length - 1 : slide - 1);
@@ -47,15 +43,15 @@ export default function ImageSlider({ url, limit = 10, page = 1 }) {
     setSlide(slide === images.length - 1 ? 0 : slide + 1);
   }
 
-  function setCurrentSlide(getIndex){
+  function setCurrentSlide(getIndex) {
     setSlide(getIndex);
   }
 
   return (
-    <div className="container">
+    <div className="relative flex justify-center items-center w-[600px] h-[450px] mx-auto">
       <BsArrowLeftCircleFill
         onClick={handlePrevious}
-        className="arrow arrow-left"
+        className="absolute left-4 w-8 h-8 text-white drop-shadow-md cursor-pointer"
       />
       {images && images.length
         ? images.map((imageItem, index) => (
@@ -63,33 +59,29 @@ export default function ImageSlider({ url, limit = 10, page = 1 }) {
               key={imageItem.id}
               alt={imageItem.url}
               src={imageItem.download_url}
-              className={
-                slide === index
-                  ? "current-image"
-                  : "current-image hide-current-image"
-              }
+              className={`rounded-lg shadow-md w-full h-full ${
+                slide === index ? "block" : "hidden"
+              }`}
             />
           ))
         : null}
       <BsArrowRightCircleFill
         onClick={handleNext}
-        className="arrow arrow-right"
+        className="absolute right-4 w-8 h-8 text-white drop-shadow-md cursor-pointer"
       />
-      <span className="circle-indicators">
+      <span className="flex absolute bottom-4 space-x-2">
         {images && images.length
           ? images.map((_, index) => (
               <button
                 key={index}
-                className={
-                  slide === index
-                    ? "current-indicator"
-                    : "current-indicator inactive-indicator"
-                }
-                onClick={()=> setCurrentSlide(index)}
+                className={`h-4 w-4 rounded-full outline-none border-none ${
+                  slide === index ? "bg-white" : "bg-gray-400"
+                }`}
+                onClick={() => setCurrentSlide(index)}
               ></button>
             ))
           : null}
       </span>
     </div>
   );
-} 
+}
